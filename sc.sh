4e6c7945546a32900090934d2695d6cfd30ac4d2
mungil95 <36015957+mungil95@users.noreply.github.com>
2018-02-01 05:36:27 +0700

    Add files via upload

---
+++ sc.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git  nano
+git clone https://github.com/mungil95/xmr-stak-cpu.git
+cd xmr-stak-cpu
+cmake .
+make install
+cd bin/
+chmod +x xmr-stak-cpu
+./xmr-stak-cpu
+sudo sysctl -w vm.nr_hugepages=128
+chmod +x xmr-stak-cpu
+echo -e "vm.nr_hugepages=128" >> /etc/sysctl.conf
+echo -e "screen -d -r" >> /root/.bashrc
+screen
